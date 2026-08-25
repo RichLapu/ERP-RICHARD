fa67f73fc2c9c0d643ff97e2ae59edd057321d1b
Richard Lapuente <119261455+RichLapu@users.noreply.github.com>
2026-08-25 00:00:20 -0300

    Refactor database connection to use environment variables

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-    host: process.env.DB_HOST || 'intranet-db.cspiueqwiu56.us-east-1.rds.amazonaws.com', 
-    user: process.env.DB_USER || 'admin',                               
-    password: process.env.DB_PASSWORD || 'senhaextremamentefortedodb',               
-    database: process.env.DB_NAME || 'intranet_db',
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USER,                               
+    password: process.env.DB_PASSWORD,               
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
